df6233048957dadde0aa85595f62a14a913cabbf
Usman Jamil <usman.jamil@live.co.uk>
2015-10-08 10:13:45 +0100

    completed plane tests and all passing

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,6 +1,5 @@
 describe("Plane", function() {
   var plane;
-  // var airport;
 
   beforeEach(function() {
     plane = new Plane();
@@ -20,5 +19,23 @@ describe("Plane", function() {
     plane.land();
     plane.take_off();
     expect(plane.isFlying).toBe(true)
-  })
+  });
+
+  it("should not be landed after take off", function() {
+    plane.land();
+    plane.take_off();
+    expect(plane.isLanded).toBe(false)
+  });
+
+
+  it("should raise error if trying to take off whilst flying", function() {
+    expect(function() {plane.take_off(plane);
+    }).toThrowError("Plane is already flying");
+  });
+
+  it("should raise error if trying to land whilst landed", function() {
+    plane.land();
+    expect(function() {plane.land(plane);
+    }).toThrowError("Plane is already landed");
+  });
 });
--- src/Plane.js
+++ src/Plane.js
@@ -1,11 +1,22 @@
 function Plane() {
     this.isFlying = true;
+    this.isLanded = false;
 };
 
 Plane.prototype.land = function() {
-  this.isFlying = false;
+  if (this.isFlying === true) {
+    this.isFlying = false;
+    this.isLanded = true;
+  } else {
+    throw Error("Plane is already landed");
+  }
 };
 
 Plane.prototype.take_off =function() {
-  this.isFlying = true;
+  if (this.isFlying === false) {
+    this.isFlying = true;
+    this.isLanded = false;
+  } else {
+    throw Error("Plane is already flying");
+  }
 };
